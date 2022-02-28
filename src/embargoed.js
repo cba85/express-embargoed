cda4d6bd0f83cdd763381889553700f35db60394
Clément Barbaza <clement.barbaza@gmail.com>
2022-02-28 17:45:06 +0100

    migrate code for package

--- src/embargoed.js
+++ src/embargoed.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
+const path = require("path");
 const { Reader } = require("maxmind");
 
-const db = "./data/Geoacumen-Country.mmdb";
-const blockedCountry = "FR";
+const db = path.join(__dirname, "../data", "Geoacumen-Country.mmdb");
+const blockedCountry = "RU"; // Russia
 
 module.exports = (req, res, next) => {
     const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
@@ -11,7 +12,7 @@ module.exports = (req, res, next) => {
     const info = lookup.get(ip);
 
     if (info && info.country.iso_code == blockedCountry) {
-        const template = "./public/maintenance.html";
+        const template = path.join(__dirname, "../public", "maintenance.html");
         const view = fs.readFileSync(template);
 
         res.setHeader("Content-Type", "text/html");
